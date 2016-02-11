03b2282261d87efa18682f6dd9da4f321c462a24
grosmar <zubak.balazs@gmail.com>
2016-02-11 18:13:59 +0100

    packaging update

--- build/task/concat-js.js
+++ build/task/concat-js.js
@@ -3,9 +3,10 @@ var path = require('path');
 var concat = require('gulp-concat');
 
 var classlistPolyfillFile = path.join(__dirname, '../../node_modules/classlist-polyfill/src/index.js');
+var hlsJsFile = path.join(__dirname, '../../node_modules/hls.js/dist/hls.js');
 
 module.exports = function() {
-  return gulp.src([classlistPolyfillFile])
+  return gulp.src([classlistPolyfillFile, hlsJsFile])
     .pipe(concat('resource.js'))
     .pipe(gulp.dest(path.join(__dirname, '../../bin/js')));
 };
